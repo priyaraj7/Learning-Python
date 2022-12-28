6244dec9c9c1c0c5be053c782f1acb854670c6fd
Priyaraj7 <suppichala@gmail.com>
2022-12-27 23:29:14 -0800

    delete: duplicate files

--- idiot.py
+++
@@ -1,9 +0,0 @@
-import pyinputplus as pyip
-
-while True:
-    prompt = "want to know how to keep an idot busy for hours?\n"
-    response = pyip.inputYesNo(prompt)
-    if response == 'no':
-        break
-
-print("Thank you. Have a nice day")
